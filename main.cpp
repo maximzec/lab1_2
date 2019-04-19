f0fdbb95a3cf231ce81b99e57b9ce5004fa3c775
Student <student@mirea.ru>
2019-04-19 14:23:04 +0300

    изменено

--- main.cpp
+++ main.cpp
@@ -2,44 +2,28 @@
 #include "complex.h"
 
 int main() {
-    bool b = true ;
-    for(;b;)
+	FILE *file;
+	int length , position;
+	file = fopen("complex.txt", "r");
+	if (file == NULL) {
+		std::cout << "cannot find file" << std::endl;
+	}
+	else {
+		fscanf(file, "%d", &length);
+		Complex *arr = new Complex[length];
+		Complex max;
+		max.setComplex(0, 0);
+		for (int i = 0; i < length; i++)
+		{
+			fscanf(file, "%lf %lf", &arr[i].real, &arr[i].imag);
+			if (arr[i].getAbs() > max.getAbs())
+			{
+				max.setComplex(arr[i].real, arr[i].imag);
+			}
+		}
 
-    {
-        Complex c1, c2;
-        char c;
-        std::cout << "enter the first complex:";
-        std::cin >> c1.real >> c1.imag;
-        std::cout << std::endl;
-        std::cout << "enter the second complex:";
-        std::cin >> c2.real >> c2.imag;
-
-        std::cout
-                << "what do you want do to with your number? : + is sum , - is difference , * is multiplication , / is dividing , 0 is exiting the programm ";
-        std::cin >> c;
-
-        switch (c) {
-            case '+':
-                c1.sumComplex(c2);
-                c1.printComplex();
-                break;
-            case '-':
-                c1.diffCompex(c2);
-                c1.printComplex();
-                break;
-            case '*':
-                c1.multComplex(c2);
-                c1.printComplex();
-                break;
-            case '/':
-                c1.divideComplex(c2);
-                c1.printComplex();
-                break;
-            case '0':
-                b = false;
-                break;
-        }
-
-    }
-    return 0;
+		max.printComplex();
+		fclose(file);
+	}
+	return 0;
 }
